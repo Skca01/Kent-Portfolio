6852f8c915aa3a1d571e95f266e89424bcd249c0
Kent Carlo Amante <144303092+Skca01@users.noreply.github.com>
2025-06-06 13:35:37 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,72 @@
+/**
+ * Kent Carlo B. Amante Portfolio - Service Worker
+ * 
+ * @author: Kent Carlo B. Amante
+ * @email: carloamante125@gmail.com
+ * @github: https://github.com/Skca01
+ * @description: Service Worker for offline functionality and caching
+ * @copyright: © 2025 Kent Carlo B. Amante. All rights reserved.
+ */
+
+const CACHE_NAME = 'portfolio-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/kent.jpg',
+    '/resume.pdf',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event Strategy (Cache First, then Network)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then((response) => {
+                        // Don't cache if not a success response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        
+                        return response;
+                    });
+            })
+    );
+}); 
